a7465c4bb3521828cbfb483593b05afd215b31c6
dhanush523 <dhanushshankar74@gmail.com>
2025-11-16 11:35:07 +0530

    Updated files

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function toggleMenu() {
     }
 
     //    <!-- -------------------------------------------------------------------------------------------- -->
+ function bookService(serviceName) {
+      const phoneNumber = "919360362776"; // replace with your WhatsApp number
+      const message = `Hello! I want to book the service: 
+    🛠️${serviceName}`;
+      const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+      window.open(url, "_blank");
+    }
+ 
