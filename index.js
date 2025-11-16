de9b980a22e8e11d4acb207f398399b36f52c57c
dhanush523 <dhanushshankar74@gmail.com>
2025-11-16 13:04:20 +0530

    Updated files

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ function toggleMenu() {
       window.open(url, "_blank");
     }
  
+    // --------------------------------------------------------------------------------------------
+
+    document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent page reload
+
+    // get form data
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let service = document.getElementById("service").value;
+    let message = document.getElementById("message").value;
+
+    let whatsappNumber = "919360362776"; // your number
+
+    // whatsapp message url
+    let url =
+      "https://wa.me/" + whatsappNumber + "?text=" +
+      "📩 *New Customer Enquiry*%0A%0A" +
+      "*Name:* " + name + "%0A" +
+      "*Phone:* " + phone + "%0A" +
+      "*Email:* " + email + "%0A" +
+      "*Service Needed:* " + service + "%0A" +
+      "*Message:* " + message + "%0A";
+
+    // open whatsapp
+    window.open(url, "_blank");
+
+    // clear form
+    document.getElementById("contactForm").reset();
+});
+
+// --------------------------------------------------------------------------------------------
